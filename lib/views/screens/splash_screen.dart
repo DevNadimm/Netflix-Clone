946b5980159b58f4cdc8b5f3c92d3ffe81033642
DevNadimm <nadimchowdhury87@gmail.com>
2024-08-15 11:24:49 +0600

    initial

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Lottie.asset("assets/anim/netflix.json"),
+        child: Lottie.asset("assets/anim/netflix.json", width: 200),
       ),
     );
   }
