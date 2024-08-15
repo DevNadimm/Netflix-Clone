bc4354d1cb7e87d1764ad70d1752df0675aba65f
DevNadimm <nadimchowdhury87@gmail.com>
2024-08-15 20:16:56 +0600

    update

---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+const String baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+const String defaultPosterUrl = '$baseImageUrl/default_poster.jpg';
+const String fallbackImageUrl = '$baseImageUrl/fallback_image.jpg';
--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -6,6 +6,30 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/logo/netflix_logo.png",
+          height: 50,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search_rounded),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              color: Colors.blue,
+              height: 27,
+              width: 27,
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          )
+        ],
+        centerTitle: false,
+      ),
       body: Center(
         child: Text("Home Screen"),
       ),
