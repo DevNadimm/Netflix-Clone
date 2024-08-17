e487274b4676ffd33f4ccef3e843a4228badcd68
DevNadimm <nadimchowdhury87@gmail.com>
2024-08-17 11:45:39 +0600

    update

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:netflix_clone/models/movie_model.dart';
+import 'package:netflix_clone/models/search_model.dart';
 import 'package:netflix_clone/models/tv_series_model.dart';
 import 'package:netflix_clone/secrets.dart';
 
 const baseUrl = 'https://api.themoviedb.org/3/';
 const key = '?api_key=$apiKey';
+const authToken = authorization;
 
 class ApiServices {
   Future<MovieModel> getUpcomingMovies() async {
@@ -20,8 +22,8 @@ class ApiServices {
         print('Successfully fetched upcoming movies.');
         return MovieModel.fromJson(data);
       } else {
-        throw http.ClientException(
-            'Failed to load upcoming movies, Status code: ${response.statusCode}');
+        throw Exception(
+            'Failed to load upcoming movies. Status code: ${response.statusCode}');
       }
     } catch (e) {
       throw Exception('An error occurred while fetching upcoming movies: $e');
@@ -41,12 +43,11 @@ class ApiServices {
         print('Successfully fetched now playing movies.');
         return MovieModel.fromJson(data);
       } else {
-        throw http.ClientException(
-            'Failed to load now playing movies, Status code: ${response.statusCode}');
+        throw Exception(
+            'Failed to load now playing movies. Status code: ${response.statusCode}');
       }
     } catch (e) {
-      throw Exception(
-          'An error occurred while fetching now playing movies: $e');
+      throw Exception('An error occurred while fetching now playing movies: $e');
     }
   }
 
@@ -60,15 +61,40 @@ class ApiServices {
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-        print('Successfully fetched now tv series.');
+        print('Successfully fetched TV series.');
         return TvSeriesModel.fromJson(data);
       } else {
-        throw http.ClientException(
-            'Failed to load now tv series, Status code: ${response.statusCode}');
+        throw Exception(
+            'Failed to load TV series. Status code: ${response.statusCode}');
       }
     } catch (e) {
-      throw Exception(
-          'An error occurred while fetching now tv series: $e');
+      throw Exception('An error occurred while fetching TV series: $e');
+    }
+  }
+
+
+  Future<SearchModel> getSearchedMovies(String query) async {
+    const String endPoint = 'search/movie';
+    final String url = '$baseUrl$endPoint?query=$query';
+
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Authorization': '$authToken',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print('Successfully fetched search results.');
+        return SearchModel.fromJson(data);
+      } else {
+        throw Exception(
+            'Failed to load search results. Status code: ${response.statusCode}, Response: ${response.body}');
+      }
+    } catch (e) {
+      throw Exception('An error occurred while fetching search results: $e');
     }
   }
 }
--- lib/views/screens/search_screen.dart
+++ lib/views/screens/search_screen.dart
@@ -1,13 +1,53 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/models/search_model.dart';
+import 'package:netflix_clone/services/api_services.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController searchController = TextEditingController();
+  ApiServices apiServices = ApiServices();
+  late SearchModel searchModel;
+
+  void onSearch(String query) {
+    apiServices.getSearchedMovies(query).then(
+          (results) {
+        setState(
+              () {
+            searchModel = results;
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Search Screen"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: CupertinoSearchTextField(
+            padding: EdgeInsets.all(10),
+            controller: searchController,
+            style: TextStyle(color: Colors.white),
+            onChanged: (value) {
+              onSearch(searchController.text.trim());
+            },
+          ),
+        ),
       ),
     );
   }
