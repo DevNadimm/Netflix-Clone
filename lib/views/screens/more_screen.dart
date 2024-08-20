aa7a4d61c60d7a9d074cff890589c3c459d64d5f
DevNadimm <nadimchowdhury87@gmail.com>
2024-08-20 13:43:38 +0600

    update

--- lib/views/screens/more_screen.dart
+++ lib/views/screens/more_screen.dart
@@ -5,9 +5,49 @@ class MoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("More Screen"),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('New & Hot'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.cast),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Container(
+                color: Colors.blue,
+                height: 27,
+                width: 27,
+                child: Center(
+                  child: Icon(Icons.person, color: Colors.white, size: 16),
+                ),
+              ),
+            ),
+            SizedBox(width: 20),
+          ],
+          bottom: TabBar(
+            indicator: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            dividerColor: Colors.transparent,
+            unselectedLabelColor: Colors.white70,
+            labelColor: Colors.black,
+            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+            tabs: [
+              Tab(
+                text: '  🍿 Coming Soon  ',
+              ),
+              Tab(text: '  🔥 Everyone\'s Watching  ')
+            ],
+          ),
+        ),
+        body: Center(
+          child: Text("More Screen"),
+        ),
       ),
     );
   }
